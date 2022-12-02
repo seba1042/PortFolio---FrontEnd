e092483ba0ca06208fc30b1b082cb29b1850a7f6
seba1042 <Sebastian.VegaCastro@alu.frt.utn.edu.ar>
2022-12-01 23:25:24 -0300

    update 1.3

--- Argentina Programa Modulo 3/Angular/SPA/src/app/app.module.ts
+++ Argentina Programa Modulo 3/Angular/SPA/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { ProfileComponent } from './componentes/profile/profile.component';
+import { BriefcaseComponent } from './componentes/briefcase/briefcase.component';
+import { AboutmeComponent } from './componentes/aboutme/aboutme.component';
+import { EducationComponent } from './componentes/education/education.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { IndexComponent } from './componentes/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    ProfileComponent,
+    BriefcaseComponent,
+    AboutmeComponent,
+    EducationComponent,
+    SkillsComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
---
+++ Argentina Programa Modulo 3/Angular/SPA/src/app/componentes/briefcase/briefcase.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-briefcase',
+  templateUrl: './briefcase.component.html',
+  styleUrls: ['./briefcase.component.css']
+})
+export class BriefcaseComponent {
+
+}
